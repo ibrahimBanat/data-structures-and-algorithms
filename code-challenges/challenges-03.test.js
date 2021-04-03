fac5b6ee34c7afa8a638d1acf38d9e9936410afd
Ibrahim Banat <ibrahim.banat.97@gmail.com>
2021-04-03 19:48:48 +0300

    solved challenge #3

--- code-challenges/challenges-03.test.js
+++ code-challenges/challenges-03.test.js
@@ -60,6 +60,16 @@ HINT: Beware... JS default is "Lexical" ordering.
 
 const sortNumbers = (arr) => {
   // Solution code here...
+  return arr.sort((a, b) => {
+    if (a < b) {
+      return -1;
+    }
+    if (a > b) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
 };
 
 /* ------------------------------------------------------------------------------------------------
