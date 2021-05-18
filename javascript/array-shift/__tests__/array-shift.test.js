fb3609e8b72c3a28640ce33051fac110a6f88c95
Ibrahim Banat <ibrahim.banat.97@gmail.com>
2021-05-18 07:00:24 +0300

    finished all unit tests

--- javascript/array-shift/__tests__/array-shift.test.js
+++ javascript/array-shift/__tests__/array-shift.test.js
@@ -2,8 +2,8 @@
 const Shift = require("../array-shift");
 
 describe("array shift", () => {
-  describe("to give output when", () => {
-    it("have the values", () => {
+  describe("to give shifted array when", () => {
+    it("when the array input is with even length", () => {
       //arrange
       let iparray = [2, 4, 6, -8];
       let numb = 5;
@@ -12,6 +12,57 @@ describe("array shift", () => {
       //assert
       expect(shifterInctance.waiter(iparray, numb)).toStrictEqual([
         2, 4, 5, 6, -8,
+      ]);
+    });
+    it("when the input array is with odd length", () => {
+      //arrange
+      let iparray = [42, 8, 15, 23, 42];
+      let num = 16;
+      //act
+      let shifterInctance = new Shift();
+      //assert
+      expect(shifterInctance.waiter(iparray, num)).toStrictEqual([
+        42, 8, 15, 16, 23, 42,
+      ]);
+    });
+    it("when the input array have a length of 0", () => {
+      //arrange
+      let iparray = [];
+      let num = 7;
+      //act
+      let shifterInctance = new Shift();
+      //assert
+      expect(shifterInctance.waiter(iparray, num)).toStrictEqual([7]);
+    });
+    it("when the input number is an array", () => {
+      //arrange
+      let iparray = [1, 2, 3, 4, 5];
+      let num = [7, 9];
+      //act
+      let shifterInctance = new Shift();
+      //assert
+      expect(shifterInctance.waiter(iparray, num)).toStrictEqual([
+        1,
+        2,
+        3,
+        [7, 9],
+        4,
+        5,
+      ]);
+    });
+    it("when the input number is not defined", () => {
+      //arrange
+      let iparray = [1, 2, 3, 4];
+      let num;
+      //act
+      let shifterInctance = new Shift();
+      //assert
+      expect(shifterInctance.waiter(iparray, num)).toStrictEqual([
+        1,
+        2,
+        undefined,
+        3,
+        4,
       ]);
     });
   });
--- javascript/array-shift/array-shift.js
+++ javascript/array-shift/array-shift.js
@@ -1,18 +1,39 @@
 "user strict";
 
+/**
+ * shiftting an array with specified input
+ * @module ArrayShift
+ */
 class ArrayShift {
+  /**
+   * calling the method that responsible for shoffting the array
+   * @param {Array} array;
+   * @param {any} num;
+   * @returns shifted array after shifting
+   */
   waiter(array, num) {
     let shifted = this.shifter(array, num);
+
     return shifted;
   }
-  index(array) {
-    return Math.ceil(array.length / 2);
-  }
-
+  /**
+   * shifting the input array against specified input
+   * @param {Array} array;
+   * @param {any} num;
+   * @returns array input array after shifting
+   */
   shifter(array, num) {
     let index = this.index(array);
-    let spliced = array.splice(index, 0, num);
-    return spliced;
+    array.splice(index, 0, num);
+    return array;
+  }
+  /**
+   * calculate the middle index for input array
+   * @param {Array} array
+   * @returns middle index of the input array
+   */
+  index(array) {
+    return Math.ceil(array.length / 2);
   }
 }
 
