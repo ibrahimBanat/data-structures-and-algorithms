5d5c7175b842d1425687292c9bc4197efbbbfcca
Ibrahim Banat <ibrahim.banat.97@gmail.com>
2021-05-19 05:41:51 +0300

    fixed typo causes the test to fail

--- javascript/array-binary-search/__tests__/array-binary-search.test.js
+++ javascript/array-binary-search/__tests__/array-binary-search.test.js
@@ -1,16 +1,13 @@
-'use strict';
+"use strict";
 
-const binarySearch = require('../array-binary-search')
+const binarySearch = require("../array-binary-search");
 
-describe('binary search', () => {
-    describe('retrun index when', () => {
-        it('array element is equal to search key or -1 if the element does not exist' () => {
-            expect(binarySearch([4, 8, 15, 16, 23, 42], 15)).toStrictEqual(2);
-            expect(binarySearch([4,8,15,16,23,42], 42)).toStrictEqual(5);
-    expect(
-        binarySearch([11,22,33,44,55,66,77], 90)
-    ).toStrictEqual(-1);
-        })
+describe("binary search", () => {
+  describe("retrun index when", () => {
+    it("array element is equal to search key or -1 if the element does not exist", () => {
+      expect(binarySearch([4, 8, 15, 16, 23, 42], 15)).toStrictEqual(2);
+      expect(binarySearch([4, 8, 15, 16, 23, 42], 42)).toStrictEqual(5);
+      expect(binarySearch([11, 22, 33, 44, 55, 66, 77], 90)).toStrictEqual(-1);
     });
-    
+  });
 });
