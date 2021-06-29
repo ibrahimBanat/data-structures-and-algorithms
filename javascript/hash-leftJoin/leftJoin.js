1a5b63b0a56e32bd104b928f19ed475966548181
Ibrahim Banat <ibrahim.banat.97@gmail.com>
2021-06-29 20:56:10 +0300

    orgnize the code and add JSdoc

--- javascript/hash-leftJoin/leftJoin.js
+++ javascript/hash-leftJoin/leftJoin.js
@@ -1,25 +1,52 @@
 "use strict";
-
-function leftJoin(left, right) {
-  if ((left && !right) || (!left && right)) {
+/**
+ * this function implements a simplified LEFT JOIN for 2 hashmaps
+ * @param {*} leftHashmap hashmap that has word strings as keys, and a synonym of the key as values.
+ * @param {*} rightHashmap hashmap that has word strings as keys, and antonyms of the key as values.
+ * @returns local array of joined values
+ */
+function leftJoin(leftHashmap, rightHashmap) {
+  if ((leftHashmap && !rightHashmap) || (!leftHashmap && rightHashmap)) {
     return null;
   }
-  let joinArr = [];
-  for (let i = 0; i < Object.keys(left.table).length; i++) {
-    let leftKey = Object.values(left.table)[i].head.value.key;
-    let leftValue = Object.values(left.table)[i].head.value.value;
-    for (let j = 0; j < Object.keys(right.table).length; j++) {
-      let rightKey = Object.values(right.table)[j].head.value.key;
-      let rightValue = Object.values(right.table)[j].head.value.value;
+  let arrOfJoin = [];
+
+  // Combine the key and corresponding values
+  // into a new data structure according to LEFT JOIN logic.
+  //looping over the firts hashmap
+  for (
+    let leftPointer = 0;
+    leftPointer < Object.keys(leftHashmap.table).length;
+    leftPointer++
+  ) {
+    //extracting the left hashmap key, value pairs
+    let leftKey = Object.values(leftHashmap.table)[leftPointer].head.value.key;
+    let leftValue = Object.values(leftHashmap.table)[leftPointer].head.value
+      .value;
+    for (
+      let rightPointer = 0;
+      rightPointer < Object.keys(rightHashmap.table).length;
+      rightPointer++
+    ) {
+      //extracting the right hashmap key, value pains
+      let rightKey = Object.values(rightHashmap.table)[rightPointer].head.value
+        .key;
+      let rightValue = Object.values(rightHashmap.table)[rightPointer].head
+        .value.value;
+      //to apply the left join concepts
+      // check if the left key is key is equal to right key of both hash maps
       if (leftKey === rightKey) {
-        joinArr.push([leftKey, leftValue, rightValue]);
+        // combine the the key and corresponding value into a local array.
+        arrOfJoin.push([leftKey, leftValue, rightValue]);
         break;
-      } else if (j === Object.keys(right.table).length - 1) {
-        joinArr.push([leftKey, leftValue, null]);
+      } else if (rightPointer === Object.keys(rightHashmap.table).length - 1) {
+        // if the right key is equal to the right key of the key as values, add  null
+        arrOfJoin.push([leftKey, leftValue, null]);
       }
     }
   }
-  return joinArr;
+  // return the local array of joined values
+  return arrOfJoin;
 }
 
 module.exports = leftJoin;
